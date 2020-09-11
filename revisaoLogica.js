8e3a3b15142ef72ce3182d7e88424e5842eb6082
fabio abrantes diniz <fabio.abrantes.diniz@gmail.com>
2020-09-11 17:31:30 -0300

    aulas 1 e 2 finaçozado

---
+++ revisaoLogica.js
@@ -0,0 +1,25 @@
+//let x;
+// variaveis
+/* const value =10;
+let x;
+x = 10 * value;
+console.log(x)
+let x = 2**100000000000000000000000000000000000000000
+console.log(x); */
+
+/* 
+for (let i = 0; i <10; i++){
+  
+  let u = x;
+  console.log(u)
+} */
+
+let name = "fabio";
+let sobrenome = "abrantes diniz";
+let nameComplete =
+  "o servidor " + name + sobrenome + "trabalhao" + "dkfdpflbmlckçlghnbn";
+nameComplete = ` 
+o servidor 
+${name} 
+${nameComplete} trabalha no ifpb `;
+console.log(nameComplete);
---
+++ sistema-lexico1.js
@@ -0,0 +1,35 @@
+"use strict";
+let count = 10;
+count = 'fabio abrantes diniz '
+console.log(count)
+
+let name = 12;
+console.log(name.toString(2));
+
+let x = 10;
+let y = x;
+console.log(y);
+// uso do this
+console.log(this);
+
+/* function square(n,n) {
+  console.log(n*n)
+ 
+}
+
+square(2,2);
+
+var name = "fabio"
+name.sobrenome = "diniz"
+console.log(typeof name) */
+
+
+
+count += 10;
+
+console.log(count);
+//comentários
+let res = '2'* 2
+console.log(res)
+
+let x = !2 
